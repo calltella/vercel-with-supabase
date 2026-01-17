dd683f91760aab577b837a4f68cf4f819ce5076f
calltella <yuichi.asa@gmail.com>
2026-01-17 07:40:56 +0000

    signin Google

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
 export const { GET, POST } = handlers
+
+
+console.log('NODE_ENV=', process.env.NODE_ENV);
+console.log('VERCEL_ENV=', process.env.VERCEL_ENV);
